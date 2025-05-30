12a625769371f9ca4c56dd574ce64d6bb818b03d
Josephy Araújo <josephyaraujo@Mac-mini-de-Josephy.local>
2025-05-29 22:27:26 -0300

    completed

--- practice/threads_cpu_io.c
+++ practice/threads_cpu_io.c
@@ -9,11 +9,20 @@
 void* cpu_thread(void* arg) {
     printf("Thread CPU-bound %ld iniciada (PID: %d)\n", (long)arg, getpid());
     
-    // Cálculo intensivo (série de Taylor para e^x)
+    //Cálculo intensivo (série de Taylor para e^x)
     double sum = 0;
     for(int i = 0; i < 10000000; i++) {
         sum += pow(1.0, i) / tgamma(i+1);
     }
+
+    //Cálculo de pi usando a fórmula de Leibniz
+    //double pi = 0.0;
+    //int n = 100000; // Número de termos para a aproximação
+    //int signal = 1;
+    //for (int i = 1; i <= n; i += 2) {
+    //    pi += signal * (4.0 / i);
+    //    signal *= -1; // Alterna o sinal a cada termo
+    //}
     
     printf("Thread CPU-bound %ld terminou (resultado: %f)\n", (long)arg, sum);
     return NULL;
@@ -29,7 +38,7 @@ void* io_thread(void* arg) {
 
 int main() {
     pid_t pid;
-    pthread_t thread_cpu1, thread_cpu2, thread_io1, thread_io2;
+    pthread_t thread_cpu1, thread_cpu2,  thread_cpu3, thread_io1, thread_io2, thread_io3;
 
     // Cria processo filho
     pid = fork();
@@ -40,16 +49,20 @@ int main() {
         // Cria threads CPU-bound
         pthread_create(&thread_cpu1, NULL, cpu_thread, (void*)1);
         pthread_create(&thread_cpu2, NULL, cpu_thread, (void*)2);
-        
+       //pthread_create(&thread_cpu3, NULL, cpu_thread, (void*)3); 
+
         // Cria threads I/O-bound
         pthread_create(&thread_io1, NULL, io_thread, (void*)1);
         pthread_create(&thread_io2, NULL, io_thread, (void*)2);
+        //pthread_create(&thread_io3, NULL, io_thread, (void*)3);
         
         // Espera todas as threads terminarem
         pthread_join(thread_cpu1, NULL);
         pthread_join(thread_cpu2, NULL);
+        //pthread_join(thread_cpu3, NULL);
         pthread_join(thread_io1, NULL);
         pthread_join(thread_io2, NULL);
+       //pthread_join(thread_io3, NULL);
         
     } else if (pid > 0) { // Processo pai
         printf("Processo pai (PID: %d)\n", getpid());
